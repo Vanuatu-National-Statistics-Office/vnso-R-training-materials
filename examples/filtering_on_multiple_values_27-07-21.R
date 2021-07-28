e5ac1490513cee9e7eda2a5ef6aca11d4c9f7ec1
JosephCrispell <crispelljoseph@gmail.com>
2021-07-28 12:45:24 +0100

    Added examples of filtering on multiple values

---
+++ examples/filtering_on_multiple_values_27-07-21.R
@@ -0,0 +1,45 @@
+#### Preparation ####
+
+# Load the required libraries
+library(dplyr) # Manipulating data - install.packages("dplyr")
+
+# Set the working directory to current file's location
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+# Read in some data
+historic_exports_file <- file.path("..", "..", "vnso-RAP-tradeStats-materials", "data", "secure", "historical_export_99_19.csv")
+historic_exports <- read.csv(historic_exports_file)
+
+#### Useful resources ####
+
+# Subsetting using single and multiple values in base R: https://stats.idre.ucla.edu/r/faq/frequently-asked-questions-about-rhow-can-i-subset-a-data-setthe-r-program-as-a-text-file-for-all-the-code-on-this-page-subsetting-is-a-very-important-component/
+# The theory behind subsetting in base R: https://crimebythenumbers.com/subsetting-intro.html
+# Subsetting (filtering) using dplyr: https://blog.exploratory.io/filter-data-with-dplyr-76cf5f1a258e
+
+#### Filtering on multiple values ####
+
+## Filtering using dplyr
+
+# Select data for bank notes only
+historic_exports_banknotes <- historic_exports %>%
+  filter(HS == "49070010")
+
+# Select commodities being exported to Australia and New Zealand
+histroci_exports_AU_NZ <- historic_exports %>%
+  filter(CTY_Dest %in% c("AU", "NZ"))
+
+## Filtering using subset() function
+
+# Select data for bank notes only
+historic_exports_banknotes <- subset(historic_exports, HS == "49070010")
+
+# Select commodities being exported to Australia and New Zealand
+histroci_exports_AU_NZ <- subset(historic_exports, CTY_Dest %in% c("AU", "NZ"))
+
+## Filtering using square brackets
+
+# Select data for bank notes only
+historic_exports_banknotes <- historic_exports[historic_exports$HS == "49070010", ]
+
+# Select commodities being exported to Australia and New Zealand
+histroci_exports_AU_NZ <- historic_exports[historic_exports$CTY_Dest %in% c("AU", "NZ"), ]
